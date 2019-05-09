96cf888b17507732e9b6aa0b127dd9a63fec6d6d
Mauricio <mauricio@Mauricios-MacBook-Pro.local>
2019-05-09 10:08:50 -0300

    middle step

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 training_file = '../data/train.p'
 validation_file= '../data/valid.p'
@@ -14,3 +15,27 @@ with open(testing_file, mode='rb') as f:
 X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
+
+print("Dataset Loaded")
+
+
+
+# TODO: Number of training examples
+n_train = np.shape(X_train)
+
+# TODO: Number of validation examples
+n_validation = np.shape(X_valid)
+
+# TODO: Number of testing examples.
+n_test = np.shape(X_test)
+
+# TODO: What's the shape of an traffic sign image?
+image_shape = X_test
+
+# TODO: How many unique classes/labels there are in the dataset.
+n_classes = X_test
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
