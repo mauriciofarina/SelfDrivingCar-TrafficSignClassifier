55ef11a30328bfcc8c88398045b48ffddef9fb6a
Mauricio <mauricio@Mauricios-MacBook-Pro.local>
2019-05-09 10:18:38 -0300

    middle step

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
+print("**********************************\n")
+
+# Dataset Files Path
 training_file = '../data/train.p'
 validation_file= '../data/valid.p'
 testing_file = '../data/test.p'
 
+# Open Dataset Files
 with open(training_file, mode='rb') as f:
     train = pickle.load(f)
 with open(validation_file, mode='rb') as f:
@@ -12,30 +17,41 @@ with open(validation_file, mode='rb') as f:
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
     
+# Load Dataset Values
 X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
-print("Dataset Loaded")
+print("Dataset Loaded\n")
 
 
+print("**********************************\n")
 
-# TODO: Number of training examples
+# Number of training examples
 n_train = np.shape(X_train)[0]
 
-# TODO: Number of validation examples
+# Number of validation examples
 n_validation = np.shape(X_valid)[0]
 
-# TODO: Number of testing examples.
+# Number of testing examples.
 n_test = np.shape(X_test)[0]
 
-# TODO: What's the shape of an traffic sign image?
+# What's the shape of an traffic sign image?
 image_shape = (np.shape(X_train)[1], np.shape(X_train)[2])
 
-# TODO: How many unique classes/labels there are in the dataset.
-n_classes = np.shape(y_train)
+# How many unique classes/labels there are in the dataset.
+n_classes = np.shape(y_train)[0]
 
 print("Number of training examples =", n_train)
 print("Number of testing examples =", n_test)
 print("Image data shape =", image_shape)
 print("Number of classes =", n_classes)
+print("\n")
+
+
+print("**********************************\n")
+
+plt.plot([0, 1, 2, 3, 4], [0, 3, 5, 9, 11])
+plt.xlabel('Months')
+plt.ylabel('Books Read')
+plt.show()
