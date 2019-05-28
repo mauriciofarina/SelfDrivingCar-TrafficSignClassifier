04a23677461a57941c715b9f5dcd3fecb3d0fb52
Mauricio <mauricio@Mauricios-MacBook-Pro.local>
2019-05-28 13:18:48 -0300

    fix

--- networks.py
+++ networks.py
@@ -6,7 +6,7 @@ from tensorflow.contrib.layers import flatten
 
 def LeNetModified(inputData, outputClasses):    
 
-    
+    '''
     conv1 = convLayer(inputData, filterShape = (5,5,6) )
     maxPool1 = maxPool(conv1)
     
@@ -24,15 +24,32 @@ def LeNetModified(inputData, outputClasses):
     #conv3B = convLayer(maxPool2B, filterShape = (3,3,10) )
     #conv4B = convLayer(conv3B, filterShape = (1,1,10) )
 
+    '''
 
-    convOutA = flatten(conv3A)
-    #convOutB = flatten(conv4B)
 
-    #convolutionOutput = tf.concat([convOutA, convOutB], 1)
+    conv1A = convLayer(inputData, filterShape = (5,5,6) )
+    maxPool1A = maxPool(conv1A)
+    conv2A = convLayer(maxPool1A, filterShape = (5,5,16) )
+    maxPool2A = maxPool(conv2A)
+    
+    conv1B = convLayer(inputData, filterShape = (3,3,10) )
+    maxPool1B = maxPool(conv1B)
+    conv2B = convLayer(maxPool1B, filterShape = (3,3,24) )
+    maxPool2B = maxPool(conv2B)
+
+
+    conv1C = convLayer(inputData, filterShape = (1,1,10) )
+
+
+    convOutA = flatten(maxPool2A)
+    convOutB = flatten(maxPool2B)
+    convOutC = flatten(conv1C)
+
+    convolutionOutput = tf.concat([convOutA, convOutB, convOutC], 1)
 
     
 
-    fullyConn1 = fullyConnectedLayer(convOutA, outputShape = 120 , dropout=False)
+    fullyConn1 = fullyConnectedLayer(convolutionOutput, outputShape = 120 , dropout=False)
 
     fullyConn2 = fullyConnectedLayer(fullyConn1, outputShape = 100 , dropout=False)
 
