e81140f9e93c8e2d00b4e1c50271a53256d3bfd5
Mauricio <mauricio@Mauricios-MacBook-Pro.local>
2019-06-05 00:25:41 -0300

    fix

--- networks.py
+++ networks.py
@@ -8,9 +8,9 @@ def neuralNetwork(inputData, outputClasses):
 
     conv1 = convLayer(inputData, filterShape = (5,5,32) , dropout=True, dropoutKeepProb = 0.9)
     maxPool1 = maxPool(conv1)
-    conv2 = convLayer(maxPool1, filterShape = (5,5,64) , dropout=False, dropoutKeepProb = 0.9)
+    conv2 = convLayer(maxPool1, filterShape = (5,5,64) , dropout=True, dropoutKeepProb = 0.9)
     maxPool2 = maxPool(conv2)
-    conv3 = convLayer(maxPool2, filterShape = (5,5,128) , dropout=False, dropoutKeepProb = 0.9)
+    conv3 = convLayer(maxPool2, filterShape = (5,5,128) , dropout=True, dropoutKeepProb = 0.9)
     
     convolutionOutput = flatten(conv3)
     
